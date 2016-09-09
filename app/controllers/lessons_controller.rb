b8ab52c9897b3d5e223f890bd337dbfeff9f13ad
sophn11 <sophianakpil@gmail.com>
2016-09-09 16:25:50 -0700

    lessons controller set up and basic routing to lesson views now established

---
+++ app/controllers/lessons_controller.rb
@@ -0,0 +1,13 @@
+class LessonsController < ApplicationController
+  def index
+    render :index
+  end
+
+  def show
+    render :show
+  end
+
+  def win
+    render :win
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
 
+  get '/', to: 'lessons#index'
+  get '/lesson', to: 'lessons#show'
+  get '/win', to: 'lessons#win'
+
+
 end
