c608183187ae7837f784ae53dd36bae44e9d63f9
sophn11 <sophianakpil@gmail.com>
2016-09-11 04:56:44 -0700

    sets views for sign up and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
 
+
   get '/', to: 'lessons#index'
   get '/lesson', to: 'lessons#show'
   get '/win', to: 'lessons#win'
 
-  get '/signup' => 'users#new'
-  post '/users' => 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+  get '/signup', to: 'users#new'
+  post '/users', to: 'users#create'
 
 
 end
