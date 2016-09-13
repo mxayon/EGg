2d6d2175d6e46dea3262d01067a6d2dd1a6f0fbd
sophn11 <sophianakpil@gmail.com>
2016-09-13 12:49:40 -0700

    added default image to learners

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def image_tag(img , options={})
+    path = "#{Rails.root}/app/assets/images/#{img}"
+    img = "egg_default.png"  unless img.present?
+    p super(img, options)
+  end
+
 end
