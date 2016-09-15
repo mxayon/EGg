805133295d012bb44b7e4578cb9b949d38db9b58
sophn11 <sophianakpil@gmail.com>
2016-09-15 10:00:02 -0700

    puts game on lessons

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -1,34 +1,34 @@
 $(document).ready(function() {
   console.log("ready!");
-  $('.ui')
-    .form({
-      fields: {
-        email: {
-          identifier  : 'email',
-          rules: [
-            {
-              type   : 'empty',
-              prompt : 'Please enter your e-mail'
-            },
-            {
-              type   : 'email',
-              prompt : 'Please enter a valid e-mail'
-            }
-          ]
-        },
-        password: {
-          identifier  : 'password',
-          rules: [
-            {
-              type   : 'empty',
-              prompt : 'Please enter your password'
-            },
-            {
-              type   : 'length[3]',
-              prompt : 'Your password must be at least 3 characters'
-            }
-          ]
-        }
-      }
-    });
+  // $('.ui.form')
+  //   .form({
+  //     fields: {
+  //       email: {
+  //         identifier  : 'email',
+  //         rules: [
+  //           {
+  //             type   : 'empty',
+  //             prompt : 'Please enter your e-mail'
+  //           },
+  //           {
+  //             type   : 'email',
+  //             prompt : 'Please enter a valid e-mail'
+  //           }
+  //         ]
+  //       },
+  //       password: {
+  //         identifier  : 'password',
+  //         rules: [
+  //           {
+  //             type   : 'empty',
+  //             prompt : 'Please enter your password'
+  //           },
+  //           {
+  //             type   : 'length[3]',
+  //             prompt : 'Your password must be at least 3 characters'
+  //           }
+  //         ]
+  //       }
+  //     }
+  //   });
 });
